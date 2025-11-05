9178a615cb25172ffe30dd50553dbf0bbe5b2f13
Riya Verma <riyaverm578@gmail.com>
2025-11-05 15:34:48 +0530

    Add files via upload

---
+++ loop java.java
@@ -0,0 +1,17 @@
+//import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   //for (int i = 100; i>=1; i++){
+	     //  System.out.println(i);
+	   //int i = 100;
+	   //while (i>=1){
+	     //  System.out.println(i);
+	       //i = i-1;
+	   int i = 100;
+	   do {
+	       System.out.println(i);
+	       i = i-1;
+	   } while( i >=1);
+}
+}
