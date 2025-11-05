aad985ef08ed693426c38eb0a85d46aed03d8208
Riya Verma <riyaverm578@gmail.com>
2025-11-05 15:33:53 +0530

    Add files via upload

---
+++ function java.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void printjava() {
+        System.out.println("hellow java");
+    }
+    public static void printname(String name) {
+        System.out.println(name);
+    }
+    public static void printsum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        printjava();
+        printname("Riya");
+        printsum(1, 2);
+        
+      
