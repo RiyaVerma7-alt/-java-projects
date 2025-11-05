d0ab57fdb08b9e6ac310c669c57d07f3be9b6887
Riya Verma <riyaverm578@gmail.com>
2025-11-05 15:30:05 +0530

    Add files via upload

---
+++ arith. opera.java.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int sum = a + b;
+		System.out.println(sum);
+		int sub = a - b;
+		System.out.println(sub);
+		int mul = a * b;
+		System.out.println(mul);
+		double c = 6;
+		double d = 4;
+		double div = a % b;
+		System.out.println(div);
+	}
+}
