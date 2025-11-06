23b61b51d948ccf193806efc4e223f1a9204df76
Riya Verma <riyaverm578@gmail.com>
2025-11-06 21:02:53 +0530

    Add files via upload

---
+++ VovelorConsonant.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class VovelorConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a character");
+        
+        char ch = sc.next().toLowerCase().charAt(0); 
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is a vowel.");
+                break;
+            default:
+                if (Character.isLetter(ch))
+                    System.out.println(ch + " is a consonant.");
+                else
+                    System.out.println("Not an alphabet character.");
+        }
+
+    }
+}
