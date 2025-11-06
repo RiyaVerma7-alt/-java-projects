70797f98132ae1957f0f20554860497a188673fc
Riya Verma <riyaverm578@gmail.com>
2025-11-06 20:22:33 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,10 @@
+public class Typeconversion {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("integer value before conversion:"+ i);
+        double D = 100.00;
+         int i2 = (int) D;
+         System.out.println("double value:"+ D);
+          System.out.println("integer value after conversion:"+ i2);
+    }
+}
