68bc78179fa1c7a6d21a682425001212ac034c6f
Riya Verma <riyaverm578@gmail.com>
2025-11-06 20:20:34 +0530

    Add files via upload

---
+++ string convert to inteager.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+          String number = "127";
+        System.out.println("enter a string:"+ number);
+       int num = Integer.parseInt(number);
+       System.out.println("after convert string to an integer value:"+num);
+       int add = num + 10;
+       System.out.println(add);
+    }
+}
