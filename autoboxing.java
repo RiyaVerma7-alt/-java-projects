3d8146e29872024b3c9a2ec0ddc1489148e9b87d
Riya Verma <riyaverm578@gmail.com>
2025-11-06 20:21:26 +0530

    Add files via upload

---
+++ autoboxing.java
@@ -0,0 +1,10 @@
+public class autoboxing {
+    public static void main(String[] args) {
+        int num1 = 20;
+        System.out.println("primitive value"+ num1);
+        int num2 = num1;
+        System.out.println("after aoutboxing:"+ num2);
+        int num3 = num2;
+        System.out.println("after unboxing:"+ num3);
+    }
+}
