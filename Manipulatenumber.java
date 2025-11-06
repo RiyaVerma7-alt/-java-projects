67f901dce286cc0c2d6051106459926cfe9f598b
Riya Verma <riyaverm578@gmail.com>
2025-11-06 21:00:25 +0530

    Add files via upload

---
+++ Manipulatenumber.java
@@ -0,0 +1,16 @@
+public class Manipulatenumber {
+    public static void main(String[] args) {
+          int a = 10;
+          System.out.println(a);
+          a += 3;
+           System.out.println(a); 
+           a -= 3;
+           System.out.println(a); 
+           a *= 3;
+           System.out.println(a);
+           a /= 3;
+           System.out.println(a); 
+           a %= 3;
+           System.out.println(a);
+    }
+}
