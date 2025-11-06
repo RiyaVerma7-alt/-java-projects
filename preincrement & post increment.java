3fa5bf469f7dafe87f9900fa84dde482678be3bc
Riya Verma <riyaverm578@gmail.com>
2025-11-06 21:00:02 +0530

    Add files via upload

---
+++ preincrement & post increment.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+          int i = 10;
+         ++i;
+          System.out.println(i);
+    }
+}
+//(post-increment)
+public class Main {
+    public static void main(String[] args) {
+          int i = 10;
+          i++;
+          System.out.println(i);
+    }
+}
